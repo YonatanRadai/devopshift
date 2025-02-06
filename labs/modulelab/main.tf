ece697f66c4357ac212a1e7a61431ca131f92a29
Your Name <you@example.com>
2025-02-06 09:17:26 +0000

    module lab with ports

--- labs/modulelab/main.tf
+++ labs/modulelab/main.tf
@@ -2,6 +2,7 @@ module "create_ec2" {
   source = "./modules"
   ami = "ami-0c02fb55956c7d316"
   machine_type = "t2.micro"
+  ingress_ports = [22, 443, 80]
 }
 
 output "output_from_module" {
--- labs/modulelab/modules/module1.tf
+++ labs/modulelab/modules/module1.tf
@@ -1,65 +1,67 @@
 provider "aws" {
- region = var.region
+  region = var.region
 }
 
 variable "region" {
- default = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "ami" {}
 
 variable "machine_type" {}
 
-
-resource "aws_security_group" "sg" {
- ingress {
-   from_port   = 22
-   to_port     = 22
-   protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
-
-
- egress {
-   from_port   = 0
-   to_port     = 0
-   protocol    = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+variable "ingress_ports" {
+  type    = list(number)
+  default = [22]
 }
 
+resource "aws_security_group" "sg" {
+  dynamic "ingress" {
+    for_each = var.ingress_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "vm" {
- ami           = var.ami
- instance_type = var.machine_type
- vpc_security_group_ids = [aws_security_group.sg.id]
+  ami                  = var.ami
+  instance_type        = var.machine_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
- tags = {
-   Name = "Yonatan-vm"
- }
+  tags = {
+    Name = "Yonatan-vm"
+  }
 }
 
 output "vm_public_ip" {
- value       = aws_instance.vm.public_ip
- description = "Public IP address of the VM"
- depends_on = [ null_resource.check_public_ip ]
+  value       = aws_instance.vm.public_ip
+  description = "Public IP address of the VM"
+  depends_on  = [null_resource.check_public_ip]
 }
 
-
 resource "null_resource" "check_public_ip" {
- provisioner "local-exec" {
-   command = <<EOT
-     if [ -z "${aws_instance.vm.public_ip}" ]; then
-       echo "ERROR: Public IP address was not assigned." >&2
-       exit 1
-       else
-       echo "We got the IP! ${aws_instance.vm.public_ip}"
-     fi
-   EOT
- }
-
-
- depends_on = [aws_instance.vm]
+  provisioner "local-exec" {
+    command = <<EOT
+      if [ -z "${aws_instance.vm.public_ip}" ]; then
+        echo "ERROR: Public IP address was not assigned." >&2
+        exit 1
+      else
+        echo "We got the IP! ${aws_instance.vm.public_ip}"
+      fi
+    EOT
+  }
+  depends_on = [aws_instance.vm]
 }
 
 output "print_ami" {
@@ -68,4 +70,8 @@ output "print_ami" {
 
 output "print_region" {
   value = var.region
+}
+
+output "open_ports" {
+  value = var.ingress_ports
 }
